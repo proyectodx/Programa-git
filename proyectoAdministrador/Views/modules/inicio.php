4f7c7899044095af8b39cba5522995714ae823f1
Arley cardozo <cardozomontenegroarley@gmail.com>
2024-04-22 09:57:08 -0500

    creacion de el inicio de los enlaces y modificacion de enlaces en nevegacion

---
+++ proyectoAdministrador/Views/modules/inicio.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>INICIO</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
--- proyectoAdministrador/Views/modules/navegacionPrincipal.php
+++ proyectoAdministrador/Views/modules/navegacionPrincipal.php
@@ -12,7 +12,7 @@
                         <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
                         <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
                     </svg>
-                    <a href="administrador/usuarioAdminPrincipal.php">Usuario</a>
+                    <a href="Views/modules/administrador/usuarioAdminPrincipal.php">Usuario</a>
                 </li>
 
                 <li class="nav-lista ">
@@ -42,7 +42,8 @@
                     </svg>
                     <a href="administrador/empleadosPrincipal.php">Empleados</a>
                 </li>
-
+ 
+                
                 <li class="nav-lista ">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="24" height="24" viewBox="2 -8 35 35" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" /><circle cx="12" cy="7.5" r=".5" fill="currentColor" />
                 </svg>
