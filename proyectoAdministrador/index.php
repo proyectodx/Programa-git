5d94bdb2c484ace43c11c3f1b6694e1d4d7c5a46
Arley cardozo <cardozomontenegroarley@gmail.com>
2024-04-22 09:09:25 -0500

    se realizan dos include_one controlador y modelo insercion de objeto controlador

--- proyectoAdministrador/index.php
+++ proyectoAdministrador/index.php
@@ -1,1 +1,9 @@
-<?php include 'Views/cabezera.php';?>
+<?php 
+/* CONTROLADORES */
+include_once 'Controllers/Controlador.php';
+
+include_once 'models/EnlacesPaginasModelo.php';
+
+$controlador = new Controlador();
+$controlador->cargarTemplate();
+?>
