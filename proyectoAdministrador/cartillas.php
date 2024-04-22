93901d41ebbd5abcae03992770f9c815ba63dedd
Arley cardozo <cardozomontenegroarley@gmail.com>
2024-04-22 01:11:56 -0500

    Archivos borrados restaurados y cartillas.php modificado.

--- proyectoAdministrador/cartillas.php
+++ proyectoAdministrador/cartillas.php
@@ -1,5 +1,1 @@
-<?php include ("plantilla/cabecera.php");?>
-<?php include ("navegacionAdmin/navegacionPrincipal.php");?>
-<?php include ("cartillas/cartillaPrincipal.php");?>
-<?php include ("cartillas/cartillaLadoNavegacion.php");?>
-<?php include ("plantilla/pie.php");?>
+<?php include 'Views/cabezera.php';?>
--- proyectoAdministrador/plantilla/pie.php
+++
@@ -1,5 +0,0 @@
-<script src="./js/plazas.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script></script>
-</body>
-</html>
