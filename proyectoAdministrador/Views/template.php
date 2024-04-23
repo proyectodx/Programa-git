7ab80d086ed5137cbd31a47d6ff70fee9d9b0ed0
Arley cardozo <cardozomontenegroarley@gmail.com>
2024-04-23 11:00:18 -0500

    inclusion de links css y unificacion de los dos php

--- proyectoAdministrador/Views/template.php
+++ proyectoAdministrador/Views/template.php
@@ -11,22 +11,18 @@
         content="width=device-width, initial-scale=1.0"> <!-- el citio es adaptable-->
         <meta name="robots" content="index, follow">
         <meta name="theme-color" content="#09f"><!---color de la pagina web externa -->
-        <link rel="icon" href="<?php echo SERVERURL; ?>/Views/img/logo.jpg">
-        <script src="<?php echo SERVERURL; ?> Views/js/config.js"></script>
+        <link rel="icon" href="<?php echo SERVERURL; ?>Views/img/logo.jpg">
         <link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/navegacionPrincipal.css">
+        <?php
+        include 'links.php'; 
+        ?>
         <title>Gastro web</title>
     </head>
     <body>
         <?php 
-        include ('Views/modules/navegacionPrincipal.php');
+        include_once 'Views/modules/navegacionPrincipal.php';
+        $controlador = new Controlador();
+        $controlador->enlacesPaginaControlador();
         ?>
-
-        <section>
-            <?php
-            $controlador = new Controlador();
-            $controlador->enlacesPaginaControlador();
-            ?>
-        </section>
-
     </body>
 </html>
