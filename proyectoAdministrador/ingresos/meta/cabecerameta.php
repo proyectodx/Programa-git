cde83a5b7ffcf1627b51e4c7ae4a7304518a0962
Arley cardozo <cardozomontenegroarley@gmail.com>
2024-04-23 10:55:49 -0500

    eliminacion de archivos basura

--- proyectoAdministrador/ingresos/meta/cabecerameta.php
+++
@@ -1,8 +0,0 @@
-    <meta charset="UTF-8"><!--Codificacion-->
-    <meta name="viewport" 
-    content="width=device-width, initial-scale=1.0"> <!-- el citio es adaptable-->
-    <meta name="robots"
-    content="index, follow">
-    <meta name="theme-color" 
-    content="#09f"><!---color de la pagina web externa -->
-    <link rel="icon" href="./imagenes/logo.jpg">
