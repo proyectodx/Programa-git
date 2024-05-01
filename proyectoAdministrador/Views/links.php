c630ae315e4676f36d948d0d44958bc4e741a80b
Arley cardozo <cardozomontenegroarley@gmail.com>
2024-05-01 18:01:08 -0500

    modificarlos a rutas absolutas

--- proyectoAdministrador/Views/links.php
+++ proyectoAdministrador/Views/links.php
@@ -1,8 +1,8 @@
-<link rel="stylesheet" href="../Views/css/estiloCartilla/cartillaLadoNavegacion.css">
-<link rel="stylesheet" href="../Views/css/estiloEmpleados/empleadosPrincipal.css">
-<link rel="stylesheet" href="../Views/css/estiloIngresos/ingresoPrincipal.css">
-<link rel="stylesheet" href="../Views/css/estiloIngresos/ingresoPanel.css">
-<link rel="stylesheet" href="../Views/css/estiloPlazas/plazasAside.css">
-<link rel="stylesheet" href="../Views/css/estiloPlazas/plazasMaquetacion.css">
-<link rel="stylesheet" href="../Views/css/estiloPlazas/plazasPrincipal.css">
-<link rel="stylesheet" href="../Views/css/estiloUsuarioAdmin/usuariosAdminPrincipal.css">
+<link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/estiloCartilla/cartillaLadoNavegacion.css">
+<link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/estiloEmpleados/empleadosPrincipal.css">
+<link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/estiloIngresos/ingresoPrincipal.css">
+<link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/estiloIngresos/ingresoPanel.css">
+<link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/estiloPlazas/plazasAside.css">
+<link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/estiloPlazas/plazasMaquetacion.css">
+<link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/estiloPlazas/plazasPrincipal.css">
+<link rel="stylesheet" href="<?php echo SERVERURL; ?>/Views/css/estiloUsuarioAdmin/usuariosAdminPrincipal.css">
