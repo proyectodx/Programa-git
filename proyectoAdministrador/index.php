abe457b91ff3ba524d76636ba2c1d61c0ba335f4
Arley cardozo <cardozomontenegroarley@gmail.com>
2024-05-14 09:47:12 -0500

    se incliye archivos de controlador y modules

--- proyectoAdministrador/index.php
+++ proyectoAdministrador/index.php
@@ -1,8 +1,13 @@
 <?php 
 /* CONTROLADORES */
 include_once 'Controllers/Controlador.php';
+include_once 'Controllers/PlazaControlador.php';
+include_once 'Controllers/ValidarDatosControlador.php';
 
+/* MODELO */
 include_once 'models/EnlacesPaginasModelo.php';
+include_once 'models/plaza.php';
+include_once 'models/PlazaDAO.php';
 
 $controlador = new Controlador();
 $controlador->cargarTemplate();
